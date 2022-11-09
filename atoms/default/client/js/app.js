0c83d744e28d381b481565a2cf0d116d57d48e8b
Pablo Gutierrez <blosky@gmail.com>
2022-11-09 12:27:52 +0000

    nothing

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -37,29 +37,52 @@ const svg = d3.select('.dorling-interactive-wrapper')
 
 
 
-let max = d3.max(world.features.map(f => +f.properties["growth_2022"]));
+let max = d3.max(world.features.map(f => +f.properties["growth_2050"]));
+let max2 = d3.max(world.features.map(f => +f.properties["growth_2100"]));
 
 const radius = d3.scaleSqrt()
     .domain([0, max])
     .range([0, 60])
 
+const radius2 = d3.scaleSqrt()
+    .domain([0, max2])
+    .range([0, 60])
 
-const simulation = d3.forceSimulation(world.features)
-    .force("x", d3.forceX(d => projection(d.geometry.coordinates)[0]))
-    .force("y", d3.forceY(d => projection(d.geometry.coordinates)[1]))
-    .force("collide", d3.forceCollide(d => 1 + radius(d.properties.growth_2022)))
+let array = []
+let array2 = []
+
+world.features.forEach(w => array.push({coordinates: w.geometry.coordinates, properties:w.properties}) )
+world.features.forEach(w => array2.push({coordinates: w.geometry.coordinates, properties:w.properties}) )
+
+console.log(array)
+console.log(array2)
+
+
+const simulation = d3.forceSimulation()
+    .nodes(array)
+    .force("x", d3.forceX(d => projection(d.coordinates)[0]))
+    .force("y", d3.forceY(d => projection(d.coordinates)[1]))
+    .force("collide", d3.forceCollide(d => 1 + radius(d.properties.growth_2050)))
+    .stop()
+
+const simulation2 = d3.forceSimulation()
+    .nodes(array2)
+    .force("x", d3.forceX(d => projection(d.coordinates)[0]))
+    .force("y", d3.forceY(d => projection(d.coordinates)[1]))
+    .force("collide", d3.forceCollide(d => 1 + radius2(d.properties.growth_2100)))
     .stop()
 
 
 for (let i = 0; i < 300; i++) {
     simulation.tick();
+    simulation2.tick();
 }
 
 svg.selectAll("circle")
-    .data(world.features)
+    .data(array2)
     .enter().append("circle")
     .attr('class', d => d.properties.NAME)
-    .attr("r", d => radius(d.properties.growth_2022))
+    .attr("r", d => radius2(d.properties.growth_2100))
     .attr("cx", d => d.x)
     .attr("cy", d => d.y)
     .attr("fill", "steelblue")
@@ -68,42 +91,18 @@ svg.selectAll("circle")
 
 
 
-const years = ['2022', '2050', '2100']
+const years = ['2050', '2100']
 
 
 const update = (year) => {
 
     console.log(year)
 
-    max = d3.max(world.features.map(f => +f.properties["growth_" + year]));
-
-    console.log(max)
-
-    radius.domain([0, max])
-
-    simulation
-    .force("x", null)
-    .force("y", null)
-    .force("collide", null);
-
-    simulation
-    .force("x", d3.forceX(d => projection(d.geometry.coordinates)[0]))
-    .force("y", d3.forceY(d => projection(d.geometry.coordinates)[1]))
-    .force("collide", d3.forceCollide(d => 1 + radius(d.properties["growth_" + year])))
-    
-
-    for (let i = 0; i < 300; i++) {
-        simulation.tick();
-
-        
-    }
 
     svg.selectAll("circle")
-    .transition()
-    .duration(100)
     .attr("r", d => radius(d.properties["growth_" + year]))
-    .attr("y", d => d.y)
-    .attr('x', d => d.x)
+    .attr("y", d => console.log(array.find(f => f.properties.ISO_A3 === d.properties.ISO_A3).x))
+    .attr('x', d => array.find(f => f.properties.ISO_A3 === d.properties.ISO_A3).y)
     
     
 }
